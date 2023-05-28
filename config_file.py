6c3386c52af736dfdab365d316cd0fde9cdd3669
thoithaui <ducthogt127@gmail.com>
2023-05-29 02:17:46 +0700

    123

--- config_file.py
+++ config_file.py
@@ -5,12 +5,11 @@ script_path = os.path.abspath(__file__)
 folder_path = os.path.dirname(script_path).replace("\\", "/")
 
 
-def metricbeat_yml(resource, logstash_host):
-    print(logstash_host)
-    source = resource + "/metricbeat.yml"
+def metricbeat_yml(source, logstash_host):
+    source = source + "/metricbeat.yml"
     f = open(source, "w+")
     f.truncate(0)  # need '0' when using r+
-    f.close
+    f.close()
 
     # Read in the file
     with open(folder_path + "/metricbeat_template.yml", "r") as file:
@@ -24,56 +23,69 @@ def metricbeat_yml(resource, logstash_host):
         file.write(filedata)
 
 
-def metricbeat_config_mysql(resource):
-    dirpath = resource + "/module/mysql/process"
-    path = resource + "/module/mysql"
-    if os.path.exists(dirpath) and os.path.isdir(dirpath):
-        shutil.rmtree(path)
-    else:
-        shutil.copytree("./mysql", path)
+def metricbeat_config_mysql(source):
+    mysql_process_path = source+"/module/mysql/process"
+    mysql_module_path = source+"/module/mysql"
+    if not os.path.exists(mysql_process_path):
+        shutil.rmtree(mysql_module_path)
+        shutil.copytree(folder_path+"/mysql", mysql_module_path)
 
 
-def metricbeat_module_mysql(resource, mysql_host):
-    source = resource + "/modules.d/mysql.yml"
+def metricbeat_module_mysql(source, mysql_host):
+    source = source + "/modules.d/mysql.yml"
     f = open(source, "w+")
     f.truncate(0)  # need '0' when using r+
-    f.close
+    f.close()
+
+    error_paths = ''
+    for i in mysql_host:
+        if error_paths == '':
+            error_paths+='"'+i.strip()+'/"'
+        else: 
+            error_paths+=',"'+i.strip()+'/"'
 
     # Read in the file
     with open(folder_path + "/metricbeat_module_mysql.yml", "r") as file:
         filedata = file.read()
 
     # Replace the target string
-    filedata = filedata.replace("$mysql-host", mysql_host)
+    filedata = filedata.replace("$mysql-host", error_paths)
 
     # Write the file out again
     with open(source, "w") as file:
         file.write(filedata)
 
 
-def metricbeat_module_mongodb(resource, mongodb_host):
-    source = resource + "/modules.d/mongodb.yml"
+def metricbeat_module_mongodb(source, mongodb_host):
+    source = source + "/modules.d/mongodb.yml"
     f = open(source, "w+")
     f.truncate(0)  # need '0' when using r+
-    f.close
+    f.close()
 
+    error_paths = ''
+    for i in mongodb_host:
+        if error_paths == '':
+            error_paths+='"'+i.strip()+'"'
+        else: 
+            error_paths+=',"'+i.strip()+'"'
+    
     # Read in the file
     with open(folder_path + "/metricbeat_module_mongodb.yml", "r") as file:
         filedata = file.read()
 
     # Replace the target string
-    filedata = filedata.replace("$mongodb-host", mongodb_host)
+    filedata = filedata.replace("$mongodb-host", error_paths)
 
     # Write the file out again
     with open(source, "w") as file:
         file.write(filedata)
 
 
-def metricbeat_module_system(resource):
-    source = resource + "/modules.d/system.yml"
+def metricbeat_module_system(source):
+    source = source + "/modules.d/system.yml"
     f = open(source, "w+")
     f.truncate(0)  # need '0' when using r+
-    f.close
+    f.close()
 
     # Read in the file
     with open(folder_path + "/metricbeat_module_system.yml", "r") as file:
@@ -84,18 +96,101 @@ def metricbeat_module_system(resource):
         file.write(filedata)
 
 
-def metricbeat_module_tomcat(resource, tomcat_host):
-    source = resource + "/modules.d/tomcat.yml"
+def metricbeat_module_tomcat(source, tomcat_host):
+    source = source + "/modules.d/tomcat.yml"
     f = open(source, "w+")
     f.truncate(0)  # need '0' when using r+
-    f.close
+    f.close()
 
+    error_paths = ''
+    for i in tomcat_host:
+        if error_paths == '':
+            error_paths+="'"+i.strip()+"'"
+        else:
+            error_paths+=",'"+i.strip()+"'"
+    
     # Read in the file
     with open(folder_path + "/metricbeat_module_tomcat.yml", "r") as file:
         filedata = file.read()
 
     # Replace the target string
-    filedata = filedata.replace("$tomcat-host", tomcat_host)
+    filedata = filedata.replace("$tomcat-hosts", error_paths)
+
+    # Write the file out again
+    with open(source, "w") as file:
+        file.write(filedata)
+        
+def filebeat_config_yml(source, tomcat_path, logstash_host):
+    source = source + "/filebeat.yml"
+    f = open(source, "w+")
+    f.truncate(0)  # need '0' when using r+
+    f.close()
+    
+    tomcat_config = ""
+    for i in tomcat_path:
+        if i!="":
+            with open(folder_path + "/filebeat_template_tomcat.yml", "r") as file:
+                conf = file.read()
+            conf = conf.replace("$logs-path", i.strip())
+            tomcat_config+=conf
+    
+    # Read in the file
+    with open(folder_path + "/filebeat_template.yml", "r") as file:
+        filedata = file.read()
+
+    # Replace the target string
+    filedata = filedata.replace("$logstash-host", logstash_host)
+    filedata = filedata.replace("$tomcat-config", tomcat_config)
+
+    # Write the file out again
+    with open(source, "w") as file:
+        file.write(filedata)
+        
+def filebeat_module_mysql(source, mysql_paths):
+    source = source + "/modules.d/mysql.yml"
+    f = open(source, "w+")
+    f.truncate(0)  # need '0' when using r+
+    f.close()
+
+    error_paths = ''
+    slow_paths = ''
+    for i in mysql_paths:
+        if i!="":
+            if error_paths == '':
+                error_paths+='"'+i.strip()+'/*error.log*"'
+                slow_paths+='"'+i.strip()+'/*slow.log*"'
+            else:
+                error_paths+=',"'+i.strip()+'/*error.log*"'
+                slow_paths+=',"'+i.strip()+'/*slow.log*"'
+    
+    
+    # Read in the file
+    with open(folder_path + "/filebeat_module_mysql.yml", "r") as file:
+        filedata = file.read()
+
+    # Replace the target string
+    filedata = filedata.replace("$error-paths", error_paths)
+    filedata = filedata.replace("$slow-paths", slow_paths)
+
+    # Write the file out again
+    with open(source, "w") as file:
+        file.write(filedata)
+    
+def logstash_conf(source, beat_port, elastic, e_user, e_pass):
+    source = source + "/config/logstash.conf"
+    f = open(source, "w+")
+    f.truncate(0)  # need '0' when using r+
+    f.close()
+
+    # Read in the file
+    with open(folder_path + "/logstash_template.conf", "r") as file:
+        filedata = file.read()
+
+    # Replace the target string
+    filedata = filedata.replace("$port", beat_port)
+    filedata = filedata.replace("$elastic", elastic)
+    filedata = filedata.replace("$user", e_user)
+    filedata = filedata.replace("$pass", e_pass)
 
     # Write the file out again
     with open(source, "w") as file:
--- config_script.py
+++ config_script.py
@@ -4,19 +4,19 @@ script_path = os.path.abspath(__file__)
 folder_path = os.path.dirname(script_path).replace("\\", "/")
 
 
-def metric_create_ps1(resource, service_name, logstash):
+def metric_create_ps1(source, service_name, logstash):
     file_run = "metricbeat.exe"
     file_config = "metricbeat.yml"
     depends_on = ""
     if logstash.strip() != "":
         depends_on = '-dependsOn "' + logstash + '"'
 
-    f = open(resource + "/metric_service.ps1", "w+")
+    f = open(source + "/metric_service.ps1", "w+")
     f.truncate(0)  # need '0' when using r+
     f.close
     # open both files
     with open(folder_path + "/metricbeat_ps1.ps1", "r") as firstfile, open(
-        resource + "/metric_service.ps1", "a"
+        source + "/metric_service.ps1", "a"
     ) as secondfile:
         # read content from first file
         for line in firstfile:
@@ -24,7 +24,7 @@ def metric_create_ps1(resource, service_name, logstash):
             secondfile.write(line)
 
     # Read in the file
-    with open(resource + "/metric_service.ps1", "r") as file:
+    with open(source + "/metric_service.ps1", "r") as file:
         filedata = file.read()
 
     # Replace the target string
@@ -34,5 +34,68 @@ def metric_create_ps1(resource, service_name, logstash):
     filedata = filedata.replace("$depends_on", depends_on)
 
     # Write the file out again
-    with open(resource + "/metric_service.ps1", "w") as file:
+    with open(source + "/metric_service.ps1", "w") as file:
+        file.write(filedata)
+        
+        
+def filebeat_create_ps1(source, service_name, logstash):
+    file_run = "filebeat.exe"
+    file_config = "filebeat.yml"
+    depends_on = ""
+    if logstash.strip() != "":
+        depends_on = '-dependsOn "' + logstash + '"'
+
+    f = open(source + "/filebeat_service.ps1", "w+")
+    f.truncate(0)  # need '0' when using r+
+    f.close
+    # open both files
+    with open(folder_path + "/filebeat_ps1.ps1", "r") as firstfile, open(
+        source + "/filebeat_service.ps1", "a"
+    ) as secondfile:
+        # read content from first file
+        for line in firstfile:
+            # append content to second file
+            secondfile.write(line)
+
+    # Read in the file
+    with open(source + "/filebeat_service.ps1", "r") as file:
+        filedata = file.read()
+
+    # Replace the target string
+    filedata = filedata.replace("$service_name", service_name)
+    filedata = filedata.replace("$file_run", file_run)
+    filedata = filedata.replace("$file_config", file_config)
+    filedata = filedata.replace("$depends_on", depends_on)
+
+    # Write the file out again
+    with open(source + "/filebeat_service.ps1", "w") as file:
+        file.write(filedata)
+
+def logstash_create_ps1(source, service_name):
+    service_path = source+"/bin"
+    app_directory = source+"/bin/logstash.bat"
+    
+    f = open(source + "/logstash_service.ps1", "w+")
+    f.truncate(0)  # need '0' when using r+
+    f.close
+    # open both files
+    with open(folder_path + "/logstash_ps1.ps1", "r") as firstfile, open(
+        source + "/logstash_service.ps1", "a"
+    ) as secondfile:
+        # read content from first file
+        for line in firstfile:
+            # append content to second file
+            secondfile.write(line)
+
+    # Read in the file
+    with open(source + "/logstash_service.ps1", "r") as file:
+        filedata = file.read()
+
+    # Replace the target string
+    filedata = filedata.replace("$serviceName", service_name)
+    filedata = filedata.replace("$servicePath", service_path)
+    filedata = filedata.replace("$AppDirectory", app_directory)
+
+    # Write the file out again
+    with open(source + "/logstash_service.ps1", "w") as file:
         file.write(filedata)
